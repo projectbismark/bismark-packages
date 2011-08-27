8c41829d54de7b3e9a3efa3a8e16c69199c7005d
Sam Burnett <sburnett@dp4.gtnoise.net>
2011-08-27 23:10:16 +0000

    Report drop statistics

--- net/passive-observer/src/main.c
+++ net/passive-observer/src/main.c
@@ -2,24 +2,47 @@
 
 #include <pcap.h>
 
-void callback (
+
+void process_packet (
         u_char *user,
         const struct pcap_pkthdr *header,
         const u_char *bytes) {
-    printf("%d ", header->len);
-    fflush(stdout);
+    static int packets_received = 0;
+    static int last_dropped = 0;
+    static struct pcap_stat statistics;
+    pcap_t *handle = (pcap_t*)user;
+    ++packets_received;
+    if (packets_received % 1000 == 0) {
+        pcap_stats (handle, &statistics);
+        printf ("%d ", statistics.ps_drop - last_dropped);
+        fflush (stdout);
+        last_dropped = statistics.ps_drop;
+    }
 }
 
 int main (int argc, char *argv[]) {
-    char *dev = argv[1];
+    char *dev;
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
 
+    if (argc != 2) {
+        fprintf (stderr, "Usage: %s <interface>\n", argv[0]);
+        return 1;
+    }
+
+    dev = argv[1];
     handle = pcap_open_live (dev, BUFSIZ, 1, 1000, errbuf);
     if (!handle) {
         fprintf (stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return 2;
     }
 
-    return pcap_loop (handle, -1, callback, NULL);
+    /* By default, pcap uses an internal buffer of 500 KB. Any packets that
+     * overflow this buffer will be dropped. pcap_stats tells the number of
+     * dropped packets.
+     *
+     * Because pcap does its own buffering, we don't need to run packet
+     * processing in a separate thread. (It would be easier to just increase
+     * the buffer size if we experience performance problems.) */
+    return pcap_loop (handle, -1, process_packet, (u_char *)handle);
 }
