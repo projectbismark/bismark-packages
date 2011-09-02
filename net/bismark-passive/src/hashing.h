431290bcb41b2705a5b794d0c8d8202d6e633832
Sam Burnett <collage@gtnoise.net>
2011-09-01 22:31:05 -0400

    oops

---
+++ net/bismark-passive/src/hashing.h
@@ -0,0 +1,19 @@
+#ifndef _BISMARK_PASSIVE_HASHING_H_
+#define _BISMARK_PASSIVE_HASHING_H_
+
+#include <stdint.h>
+
+#define FNV_OFFSET_BASIS 0x811c9dc5
+static uint32_t fnv_hash_32(const char* data, int len) {
+  const unsigned char *bp = (const unsigned char *)data;
+  const unsigned char *be = bp + len;
+  uint32_t hval = FNV_OFFSET_BASIS;
+
+  while (bp < be) {
+    hval += (hval<<1) + (hval<<4) + (hval<<7) + (hval<<8) + (hval<<24);
+    hval ^= *bp++;
+  }
+  return hval;
+}
+
+#endif
