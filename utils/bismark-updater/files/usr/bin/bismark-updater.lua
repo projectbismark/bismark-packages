612b385f8fe7a4cbb9347929761eaa6f7ac5556a
Sam Burnett <srburnet@gmail.com>
2012-01-05 00:15:19 +0000

    Reimplement bismark-updater in Lua

---
+++ utils/bismark-updater/files/usr/bin/bismark-updater.lua
@@ -0,0 +1,38 @@
+#!/usr/bin/env lua
+
+require('os')
+require('bmlua.opkg')
+opkg = bmlua.opkg
+require('bmlua.path')
+path = bmlua.path
+require('bmlua.set')
+set = bmlua.set
+require('nixio.fs')
+
+UPGRADABLE_CACHE = "/var/bismark-upgradable"
+
+local get_managed_repositories = function()
+    nixio.fs.mkdir(UPGRADABLE_CACHE)
+    local managed_repositories = set.Set()
+    for info in opkg.get_package_list_urls():iter() do
+        local upgradable_url = path.join(info.url, "Upgradable")
+        if os.execute("curl -s -f " .. upgradable_url) == 0 then
+            managed_repositories:add(info.name)
+        end
+    end
+    return managed_repositories
+end
+
+function main(arg)
+    os.execute("opkg update")
+
+    local upgradable = opkg.list_upgradable()
+    local managed_repositories = get_managed_repositories()
+    local candidates = opkg.get_packages_in_repositories(managed_repositories)
+    for package in upgradable:intersection(candidates):iter() do
+        print('Upgrading ' .. package)
+        opkg.upgrade(package)
+    end
+end
+
+return main(arg)
