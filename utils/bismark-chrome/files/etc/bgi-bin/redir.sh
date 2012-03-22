cf5f24b7185c3bf4678d73a0b01e94f061d8fd55
Stephen Woodrow <srwoodrow@gmail.com>
2012-03-22 15:21:08 -0400

    Rename pkg bismark-chrome-new to bismark-chrome.

--- utils/bismark-chrome/files/etc/bgi-bin/redir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Content-Type: text/html; charset=UTF-8
-Pragma: no-cache
-"
-
-echo "<html><head><meta http-equiv=\"REFRESH\" content=\"0;url=http://${SERVER_NAME}:81\"></head><body>Redirecting</body></html>"
