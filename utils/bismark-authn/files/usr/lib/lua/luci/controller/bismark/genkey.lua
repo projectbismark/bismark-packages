7e908d14e0da512376b4ac9f7d70d07cee4cb3f2
Andrew Kahn <xakahnx@gmail.com>
2012-07-19 13:29:17 -0400

    Updated script

--- utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
+++ utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
@@ -5,10 +5,9 @@ require("os")
 function index()
 	entry({"oauth", "genkey"}, template("bismark/genkey"), "redirect", 20).dependent=false
 	local token = luci.http.formvalue("token")
-	local client = luci.http.formvalue("client")
-	if not (token == nil and client == nil) then
+	if not (token == nil) then
 		local file = assert(io.open("/tmp/bismark/authn", "w"))
-		file:write("http://projectbismark.net/check/?token=" .. token .. "&client=" .. client)
+		file:write("http://projectbismark.net/check/?bearer_token=" .. token)
 		os.execute("/etc/init.d/nodogsplash stop &>/dev/null")
 		luci.http.redirect("/")
 	end
