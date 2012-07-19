36343b2a064ae5e37b0802df98270c2a82c16ce4
Andrew Kahn <xakahnx@gmail.com>
2012-07-19 16:22:32 -0400

    minor bug fixes

--- utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
+++ utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
@@ -1,4 +1,4 @@
-module("luci.controller.bismark-oauth.genkey", package.seeall)
+module("luci.controller.bismark.genkey", package.seeall)
 
 require("os")
 
@@ -7,7 +7,7 @@ function index()
 	local token = luci.http.formvalue("token")
 	if not (token == nil) then
 		local file = assert(io.open("/tmp/bismark/authn", "w"))
-		file:write("http://projectbismark.net/check/?bearer_token=" .. token)
+		file:write("https://register.projectbismark.net/check/?bearer_token=" .. token)
 		os.execute("/etc/init.d/nodogsplash stop &>/dev/null")
 		luci.http.redirect("/")
 	end
