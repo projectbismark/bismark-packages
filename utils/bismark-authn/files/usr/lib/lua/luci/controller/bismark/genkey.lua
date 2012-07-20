35e3791480ae4b2d0223339bac80586d5ed42a1f
Andrew Kahn <xakahnx@gmail.com>
2012-07-20 13:09:29 -0400

    token store moved from /tmp to /etc

--- utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
+++ utils/bismark-authn/files/usr/lib/lua/luci/controller/bismark/genkey.lua
@@ -6,7 +6,7 @@ function index()
 	entry({"oauth", "genkey"}, template("bismark/genkey"), "redirect", 20).dependent=false
 	local token = luci.http.formvalue("token")
 	if not (token == nil) then
-		local file = assert(io.open("/tmp/bismark/authn", "w"))
+		local file = assert(io.open("/etc/bismark/authn", "w"))
 		file:write("https://register.projectbismark.net/check/?bearer_token=" .. token)
 		os.execute("/etc/init.d/nodogsplash stop &>/dev/null")
 		luci.http.redirect("/")
