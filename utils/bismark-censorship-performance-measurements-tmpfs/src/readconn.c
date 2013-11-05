d30bdcfb2e2e68f105c71358d885f555f855a572
Ben Jones <benjam2@g.clemson.edu>
2013-11-05 13:39:38 -0500

    Added multiple connection functionality
    Added functionality to the program to print the output from each
    connection when multiple connections are specified

--- utils/bismark-censorship-performance-measurements-tmpfs/src/readconn.c
+++ utils/bismark-censorship-performance-measurements-tmpfs/src/readconn.c
@@ -13,75 +13,82 @@ int main(int argc, char *argv[])
 
     char buf[8];
     int  cid;
-    if (argc != 2 ) {
+    int index = 1;
+
+    if (argc < 2 ) {
         fprintf(stdout, "Usage %s <connection_id>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-
+    
     // initialize the stat measurement stuff
     if ((agent = web100_attach(WEB100_AGENT_TYPE_LOCAL, NULL)) == NULL) {
         web100_perror("web100_attach");
         web100_detach(agent);
         exit(EXIT_FAILURE);
     }
-    cid  = atoi(argv[1]);
-    conn = web100_connection_lookup(agent, cid);
-    if(conn == NULL){
-        fprintf(stdout, "Error creating connection to stats files. Are you root?\n");
-        web100_detach(agent);
-        exit(EXIT_FAILURE);
-    }
-    web100_get_connection_spec(conn, &spec);
-    {
-        unsigned char *src = (unsigned char *)&spec.src_addr;
-        unsigned char *dst = (unsigned char *)&spec.dst_addr;
-        printf("Connection %d (%u.%u.%u.%u:%u %u.%u.%u.%u:%u)\n",
-               cid,
-               src[0], src[1], src[2], src[3], spec.src_port,
-               dst[0], dst[1], dst[2], dst[3], spec.dst_port);
-    }
 
-    group = web100_group_head(agent); // begin at the head
+    // if we get more than one connection id, then get the stats for all
+    while(index < argc){
+	cid  = atoi(argv[index]);
+	index++;
+	conn = web100_connection_lookup(agent, cid);
+	if(conn == NULL){
+	    fprintf(stdout, "Error creating connection to stats files. Are you root?\n");
+	    web100_detach(agent);
+	    exit(EXIT_FAILURE);
+	}
+	web100_get_connection_spec(conn, &spec);
+	{
+	    unsigned char *src = (unsigned char *)&spec.src_addr;
+	    unsigned char *dst = (unsigned char *)&spec.dst_addr;
+	    printf("Connection %d (%u.%u.%u.%u:%u %u.%u.%u.%u:%u)\n",
+		   cid,
+		   src[0], src[1], src[2], src[3], spec.src_port,
+		   dst[0], dst[1], dst[2], dst[3], spec.dst_port);
+	}
 
-    while (group) {                   // loop through all the groups
+	group = web100_group_head(agent); // begin at the head
 
-        web100_var      *var;
-        web100_snapshot *snap;
+	while (group) {                   // loop through all the groups
 
-        printf("Group \"%s\"\n", web100_get_group_name(group));
+	    web100_var      *var;
+	    web100_snapshot *snap;
 
-        if ((snap = web100_snapshot_alloc(group, conn)) == NULL) {
-            web100_detach(agent);
-            web100_perror("web100_snapshot_alloc");
-            exit(EXIT_FAILURE);
-        }
+	    printf("Group \"%s\"\n", web100_get_group_name(group));
 
-        if (web100_snap(snap)) {
-            perror("web100_snap");
-            if (web100_errno == WEB100_ERR_NOCONNECTION)
-                continue;
-            web100_snapshot_free(snap);
-            web100_detach(agent);
-            exit(EXIT_FAILURE);
-        }
+	    if ((snap = web100_snapshot_alloc(group, conn)) == NULL) {
+		web100_detach(agent);
+		web100_perror("web100_snapshot_alloc");
+		exit(EXIT_FAILURE);
+	    }
 
-        var = web100_var_head(group);
-        while (var) {
-            if (web100_snap_read(var, snap, buf)) {
-                web100_perror("web100_snap_read");
-                web100_snapshot_free(snap);
-                web100_detach(agent);
-                exit(EXIT_FAILURE);
-            }
+	    if (web100_snap(snap)) {
+		perror("web100_snap");
+		if (web100_errno == WEB100_ERR_NOCONNECTION)
+		    continue;
+		web100_snapshot_free(snap);
+		web100_detach(agent);
+		exit(EXIT_FAILURE);
+	    }
 
-            printf("%-20s %s\n", web100_get_var_name(var),
-                   web100_value_to_text(web100_get_var_type(var), buf));
-            var = web100_var_next(var);
-        }
-        web100_snapshot_free(snap);
-	group = web100_group_next(group);
-    }
+	    var = web100_var_head(group);
+	    while (var) {
+		if (web100_snap_read(var, snap, buf)) {
+		    web100_perror("web100_snap_read");
+		    web100_snapshot_free(snap);
+		    web100_detach(agent);
+		    exit(EXIT_FAILURE);
+		}
+
+		printf("%-20s %s\n", web100_get_var_name(var),
+		       web100_value_to_text(web100_get_var_type(var), buf));
+		var = web100_var_next(var);
+	    }
+	    web100_snapshot_free(snap);
+	    group = web100_group_next(group);
+	}
     web100_detach(agent);
+    }
     printf("\n");
     return 0;
 }
